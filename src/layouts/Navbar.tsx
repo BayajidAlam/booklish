a0403f70d21d1a2126cd7511853f7f28b7a8a6eb
BayajidAlam <bayajidalam2001@gmail.com>
2023-07-20 21:47:32 +0600

    shadcn ui installed

--- src/layouts/Navbar.tsx
+++ src/layouts/Navbar.tsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React from "react";
+import logo from "../assets/logo.png";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { MdOutlineProductionQuantityLimits } from "react-icons/md";
+import { MdOutlineLogin } from "react-icons/md";
+import { RiAccountCircleLine } from "react-icons/ri";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <div className="lg:container md:w-[90%] w-[90%] mx-auto flex justify-between items-center">
+      <Link to="/">
+        <img className="h-16" src={logo} alt="" />
+      </Link>
+      <div className="flex items-center justify-center gap-4">
+        <div className="text-2xl">
+          <AiOutlineShoppingCart />
+        </div>
+        <Link to="/products" className="text-2xl">
+          <MdOutlineProductionQuantityLimits />
+        </Link>
+        <div className="text-2xl">
+          <RiAccountCircleLine />
+        </div>
+        <div className="text-2xl">
+          <MdOutlineLogin />
+        </div>
+      </div>
+    </div>
+  );
 }
